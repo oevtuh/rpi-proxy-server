d95c897fd8984b11d0d903798f11e6e85efce016
oevtuh <oevtuh@gmail.com>
2015-04-22 23:28:53 +0300

    update for testing jenkins

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         console.log(msg);
-        io.emit('chat message', msg);
+        io.emit('chat message', msg+" s");
     });
 });
 
